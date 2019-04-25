f3e99932647b770f20fac0e51933bad74c3fea97
tangbing <tangbing@tangbingdeiMac.local>
2019-04-25 23:54:34 +0800

    m

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,5 +1,5 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Button } from '@tarojs/components'
 
 export default class Index extends Component {
 
@@ -17,10 +17,32 @@ export default class Index extends Component {
 
   componentDidHide () { }
 
+  click(e){
+    e.stopPropagation()
+
+    Taro.switchTab({
+      url: '/pages/index/index'
+    })
+
+    Taro.showToast({
+      title: '成功',
+      icon: 'success',
+      duration: 2000
+    })
+      .then(res => console.log(res))
+  }
+  click2(e){
+    e.stopPropagation()
+    Taro.navigateTo({
+      url: '/pages/content/taobao'
+    })
+  }
   render () {
     return (
       <View className='index'>
-        <Text>个人中心</Text>
+        <Text>我们 中心啊</Text>
+        <Button onClick={this.click}>点击下试试</Button>
+        <Button onClick={this.click2}>点击下试试</Button>
       </View>
     )
   }
