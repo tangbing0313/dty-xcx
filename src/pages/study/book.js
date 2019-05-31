c45ae1f4ed35a3c9ead4b7ba39f57e5b6a992d94
tangbing <tangbing@chinasosmart.com>
2019-05-31 20:03:31 +0800

    xx

--- src/pages/study/book.js
+++ src/pages/study/book.js
@@ -1,6 +1,7 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Text, Button } from '@tarojs/components'
 import './book.css'
+import '../../assets/icon/close.png';
 
 export default class Book extends Component {
 
@@ -24,12 +25,20 @@ export default class Book extends Component {
       <View className='book-wapper'>
         <View className='chapter'>
           <View className='chapter-list f'>
-            <View className='chapter-l-l f'>时代峰峻时代峰峻时代峰峻时代峰峻时代峰峻</View>
-            <View className='chapter-l-r f'>sdf</View>
+            <View className='chapter-l-l f'><Text className='chapter-t'>1-2</Text> Excuse me!</View>
+            <View className='chapter-l-r f'><image className='chapter-img' src={require('../../assets/icon/close.png')} /></View>
           </View>
           <View className='chapter-list f'>
-            <View className='chapter-l-l f'>时代峰峻时代峰峻时代峰峻时</View>
-            <View className='chapter-l-r f'>sdf</View>
+            <View className='chapter-l-l f'>3-4: Sorry sir.</View>
+            <View className='chapter-l-r f'><image className='chapter-img' src={require('../../assets/icon/close.png')} /></View>
+          </View>
+          <View className='chapter-list f'>
+            <View className='chapter-l-l f'>Lesson 5-6 Nice to meet you</View>
+            <View className='chapter-l-r f'><image className='chapter-img' src={require('../../assets/icon/close.png')} /></View>
+          </View>
+          <View className='chapter-list f'>
+            <View className='chapter-l-l f'>Lesson 5-6 Mrs. Smith’s living room</View>
+            <View className='chapter-l-r f'><image className='chapter-img' src={require('../../assets/icon/close.png')} /></View>
           </View>
           <View className='chapter-list'>章节名称</View>
           <View className='chapter-list'>章节名称</View>
